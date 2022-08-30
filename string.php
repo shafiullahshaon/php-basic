4299bc8470b306a390923ad7e2f65216bd157eb0
shafiullahshaon <ss.raj.bd@gmail.com>
2022-08-30 19:21:32 +0600

    some string function

---
+++ string.php
@@ -0,0 +1,21 @@
+<?php
+
+$str = "Hello Bangladesh";
+
+echo strlen($str);
+
+echo "<br>";
+
+echo str_word_count($str);
+
+echo "<br>";
+
+echo strrev($str);
+
+echo "<br>";
+
+echo strpos($str, "Hello");
+
+echo "<br>";
+
+echo str_replace("Bangladesh", "Friend", $str);
